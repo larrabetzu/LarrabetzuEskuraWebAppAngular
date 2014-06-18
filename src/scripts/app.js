1024cd9c3f7e4967ec2ed955948f33f09a31292b
Sergio Peña <sergiop79@gmail.com>
2014-06-18 19:50:36 +0200

    fondo eta footer aldaketa rss ikonoarekin

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,26 +4,26 @@ angular.module('EskuraApp', ['ngRoute', 'ngResource'])
   .config(['$routeProvider', function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl : '../views/main.html'
+        templateUrl : 'views/main.html'
       })
       .when('/agenda', {
-        templateUrl: '../views/agenda.html',
+        templateUrl: 'views/agenda.html',
         controller: 'AgendaCtrl'
       })
       .when('/albisteak', {
-        templateUrl: '../views/albisteak.html',
+        templateUrl: 'views/albisteak.html',
         controller: 'AlbisteakCtrl'
       })
       .when('/ekintza/:pk', {
-        templateUrl: '../views/ekintza.html',
+        templateUrl: 'views/ekintza.html',
         controller: 'EkintzaCtrl'
       })
       .when('/elkarteak', {
-        templateUrl: '../views/elkarteak.html',
+        templateUrl: 'views/elkarteak.html',
         controller: 'ElkarteakCtrl'
       })
       .when('/twitter', {
-        templateUrl: '../views/twitter.html'
+        templateUrl: 'views/twitter.html'
       })
       .otherwise({
         redirectTo: '/'
