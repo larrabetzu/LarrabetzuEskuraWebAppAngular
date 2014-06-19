156586b6164e9dabbcb71d1c361cc129d01d4c90
Sergio Peña <sergiop79@gmail.com>
2014-06-19 12:47:31 +0200

    Albisteen jarioen ordena aldatuta

--- src/scripts/controllers/albisteak.js
+++ src/scripts/controllers/albisteak.js
@@ -3,4 +3,5 @@
 angular.module('EskuraApp')
   .controller('AlbisteakCtrl', ['$scope', 'FeedList', function ($scope, FeedList) {
     $scope.feeds = FeedList.get();
+    console.log($scope.feeds);
   }]);
--- src/scripts/services/feedlist.js
+++ src/scripts/services/feedlist.js
@@ -6,11 +6,11 @@ angular.module('EskuraApp')
     this.get = function () {
       var feedSources = [
         {title: 'Larrabetzutik', url: 'http://larrabetzutik.org/feed/'},
-        {title: 'Hori Bai', url: 'http://horibai.org/feed/rss/'},
-        {title: 'iPtx', url: 'http://www.larrabetzu.org/gaztelumendi/?feed=rss2'},
-        {title: 'Larrabetzuko Eskola', url: 'http://www.larrabetzukoeskola.org/feed/'},
         {title: 'Larrabetzuko Udala', url: 'http://www.larrabetzuko-udala.com/_layouts/feed.aspx?xsl=1&web=%2Feu-ES&page=80690b0d-69fd-4e54-901d-309ace29e156&wp=e062f3df-e82b-4a0f-9365-2aefefa7a8a5'},
-        {title: 'Larrabetzu Zero Zabor', url: 'http://larrabetzuzerozabor.org/index.php/eu/bloga?format=feed&type=rss'}
+        {title: 'Larrabetzuko Eskola', url: 'http://www.larrabetzukoeskola.org/feed/'},
+        {title: 'Hori Bai', url: 'http://horibai.org/feed/rss/'},
+        {title: 'Larrabetzu Zero Zabor', url: 'http://larrabetzuzerozabor.org/index.php/eu/bloga?format=feed&type=rss'},
+        {title: 'iPtx', url: 'http://www.larrabetzu.org/gaztelumendi/?feed=rss2'}
       ];
       
       if (feeds.length === 0) {
@@ -19,10 +19,10 @@ angular.module('EskuraApp')
             var feed = data.responseData.feed;
             // zero zabor eta udalaren titulu aldaketa
             if (feed.title === 'Bloga') {
-              feed.title = 'Larrabetzu Zero Zabor'
+              feed.title = 'Zero Zabor'
             };
             if (feed.title === 'Suscripciones') {
-              feed.title = 'Larrabetzuko Udala'
+              feed.title = 'Udala'
             };
             
             feeds.push(feed);
